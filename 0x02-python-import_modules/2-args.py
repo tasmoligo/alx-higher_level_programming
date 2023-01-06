52649e44f8e0492355d6fdf38158a06eaed737c7
Tasie Ibiam <tasieibiam28@gmail.com>
2023-01-05 16:21:51 -0800

    All day all night

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("0 argument.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n))
+    for i in range(n):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
