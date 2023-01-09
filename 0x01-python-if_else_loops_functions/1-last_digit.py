fb4424f5df842dd3afc6309b6f5b3c6c23d63d6e
Tasie Ibiam <tasieibiam28@gmail.com>
2023-01-09 15:25:20 -0800

    Task 1 corrected

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
-    n = number % 10
+n = abs(number) % 10
+if number < 0:
+    n = -n
+    print(f"Last digit of {number:d} is {n:d} and is", end=" ")
     if n > 5:
-        print(f"Last digit of {number:d} is {n:d} and is greater than 5")
+        print(F"greatr than 5")
     elif n == 0:
-        print(f"Last digit of {number:d} is {n:d} and is 0")
+        print(F"0")
     else:
-        print(f"Last digit of {number:d} is {n:d} and is less than 6 and not 0")
+        print(F"less than 6 and not 0")
 elif number == 0:
-    print(f"Last digit of {number:d} is {number % 10:d} and is 0")
+    print(f"Last digit of {number:d} is {n:d} and is 0")
 else:
-    i = number % 10
-    if i == 0:
-        print("Last digit of {number:d} is {i:d} and is 0")
+    print(f"Last digit of {number:d} is {n:d} and is", end=" ")
+    if n > 5:
+        print(F"greatr than 5")
+    elif n == 0:
+        print(F"0")
     else:
-        print(f"Last digit of {number:d} is {i:d} and is less than 6 and not 0")
+        print(F"less than 6 and not 0")
