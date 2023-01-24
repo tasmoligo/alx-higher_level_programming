b78de5786e5abba2ccda9ce815d4134ae862fd10
Tasie Ibiam <tasieibiam28@gmail.com>
2023-01-24 04:05:18 -0800

    Task 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     answer = 0
     for i in range(1, 3):
@@ -8,6 +9,6 @@ def magic_calculation(a, b):
             else:
                 answer += a ** b / i
         except:
-            answer = a + b
+            answer = b + a
             break
     return answer
