9719bea05454ec19c9220a42aae672d16a2b8673
Tasie Ibiam <tasieibiam28@gmail.com>
2023-01-23 21:47:41 -0800

    Task 9

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    answer = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                answer += a ** b / i
+        except:
+            answer = a + b
+    return answer
