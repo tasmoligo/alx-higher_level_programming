9a5a4e94f33872733b55bdb1bf6bf6e1878e43f6
Tasie Ibiam <tasieibiam28@gmail.com>
2023-02-14 04:41:22 -0800

    Square update: args and kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,40 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        '''Public method that assigns attributes
+        arguments:
+            *args - positional argument
+            **kwargs - key-word argument
+        '''
+        if args and len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    if args[i] is None:
+                        self.__init__(self.height, self.height, self.x, self.y)
+                    else:
+                        self.id = args[i]
+                if i == 1:
+                    self.width = args[i]
+                    self.height = args[i]
+                if i == 2:
+                    self.x = args[i]
+                if i == 3:
+                    self.y = args[i]
+        else:
+            if kwargs and len(kwargs) != 0:
+                for k, v in kwargs.items():
+                    if k == "id":
+                        if v is None:
+                            self.__init__(
+                                    self.height, self.height, self.x, self.y)
+                        else:
+                            self.id = v
+                    elif k == "size":
+                        self.width = v
+                        self.height = v
+                    elif k == "x":
+                        self.x = v
+                    elif k == "y":
+                        self.y = v
