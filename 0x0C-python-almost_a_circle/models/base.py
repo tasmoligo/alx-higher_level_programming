8d99953acfd5990eec20ca4a8d0599772cd35c84
Tasie Ibiam <tasieibiam28@gmail.com>
2023-02-14 08:08:19 -0800

    json.loads

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,10 @@ class Base:
             else:
                 list_dicts = [i.to_dictionary() for i in list_objs]
                 f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''returns the list of the JSON string representation json_string'''
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
