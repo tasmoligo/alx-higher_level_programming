416d310ad2d21c6319c72b72d0437289d43f2ad8
Tasie Ibiam <tasieibiam28@gmail.com>
2023-02-14 08:00:44 -0800

    jsonnnngit add .!

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''
 Defines the Base class
 '''
+import json
 
 
 class Base:
@@ -22,5 +23,23 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Returns the JSON string representation of list_dictionaries'''
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''writes the JSON string representation of list_objs to a file'''
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w", encoding="utf-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [i.to_dictionary() for i in list_objs]
+                f.write(Base.to_json_string(list_dicts))
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -132,3 +132,8 @@ class Rectangle(Base):
                         self.x = v
                     if k == "y":
                         self.y = v
+
+    def to_dictionary(self):
+        '''returns the dictionary representation of a Rectangle'''
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
