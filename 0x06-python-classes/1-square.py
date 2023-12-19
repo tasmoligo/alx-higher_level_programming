966b46bfae22ca2a07c207361e06cf1a42057558
Tasie Ibiam <tasieibiam28@gmail.com>
2023-12-19 12:44:26 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Defines a square'''
+
+
+class Square:
+    '''The square class'''
+    def __init__(self, size):
+        '''Instantiates a square with size'''
+        self.__size = size
