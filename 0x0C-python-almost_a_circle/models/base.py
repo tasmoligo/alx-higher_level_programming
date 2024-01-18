81eecb7ab277805f3917123b305ec0c568e0f273
Tasie Ibiam <tasieibiam28@gmail.com>
2024-01-18 07:46:58 -0800

    python inheritance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Module for class """
 
+import json
+
 
 class Base:
     """base class"""
@@ -13,3 +15,24 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns the JSON string representation
+        of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list_objs to a file. """
+        filename = __class__.__name__ + ".json"
+        with open(filename, "w") as openfile:
+            if list_objs is None:
+                openfile.write("[]")
+            else:
+                for arg in list_objs:
+                    json_string = arg.to_dictionary()
+                openfile.write(Base.to_json_string(json_string))
