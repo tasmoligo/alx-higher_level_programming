25b338c6e8e1948e96d830026d6d6d5e4468628c
Tasie Ibiam <tasieibiam28@gmail.com>
2024-03-11 23:32:19 +0100

    JavaScript warmup

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const arg = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i;
+
+for (i = 0; i < arg.length; i++)
+{
+	console.log(arg[i]);
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let i;
+const arg = process.argv[2];
+if (parseInt(arg))
+{
+	for (i = 0; i < arg; i++)
+	{
+		console.log("C is fun");
+	}
+}
+else
+{
+	console.log("Missing number of occurrences");
+}
