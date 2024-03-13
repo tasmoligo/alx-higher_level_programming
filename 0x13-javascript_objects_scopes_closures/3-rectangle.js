843e6eab0b2bf7baf2b41b926f085a5dd28abeac
Tasie Ibiam <tasieibiam28@gmail.com>
2024-03-13 14:02:31 +0100

    More JavaScript

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// creates a Rectangle class
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let shape = '';
+      for (let j = 0; j < this.width; j++) {
+        shape += 'X';
+      }
+      console.log(shape);
+    }
+  }
+}
+module.exports = Rectangle;
