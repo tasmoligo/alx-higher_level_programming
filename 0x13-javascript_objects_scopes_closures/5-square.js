f16eab9b0781e11af541a64e5726845661eeffca
Tasie Ibiam <tasieibiam28@gmail.com>
2024-03-13 22:13:36 +0100

    More JavaScript

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Creates a class Square that inherits from Rectangle
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+}
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Creates a class Square that inherits from Rectangle
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  /* prints the rectangle using the character c
+   * - If c is undefined, use the character X
+   */
+  charPrint (c) {
+    const character = c === undefined ? 'X' : 'C';
+    for (let i = 0; i < this.width; i++) {
+      let printout = '';
+      for (let j = 0; j < this.height; j++) {
+        printout += character;
+      }
+      console.log(printout);
+    }
+  }
+}
+module.exports = Square;
