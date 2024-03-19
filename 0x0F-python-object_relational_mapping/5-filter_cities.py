f6496fd097913963d5e14adc52a225d5ecc77037
Tasie Ibiam <tasieibiam28@gmail.com>
2024-03-19 12:48:02 -0700

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,13 +10,10 @@ from sys import argv
 
 if __name__ == "__main__":
     """
-    Access to the database and get the cities
-    from the database.
+    Connect to the database and extract required data
     """
-
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
-
     with db_connect.cursor() as db_cursor:
         db_cursor.execute("""
                           SELECT cities.id, cities.name
