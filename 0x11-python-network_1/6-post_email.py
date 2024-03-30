817230d98f97fe7d7c5e4e056e2dc6718069bc4e
Tasie Ibiam <tasieibiam28@gmail.com>
2024-03-30 17:02:57 +0100

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+    response = requests.post(url, data=email)
+    print(response.text)
