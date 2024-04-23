2dd6df1605ce0f340229b651aff3453817d9ed04
Tasie Ibiam <tasieibiam28@gmail.com>
2024-04-24 00:28:57 +0100

    JavaScript

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (err, response, body) => {
+request(url, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
