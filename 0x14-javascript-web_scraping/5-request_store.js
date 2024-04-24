0ca597f31af92b715544c342efc108e661e3df31
Tasie Ibiam <tasieibiam28@gmail.com>
2024-04-24 22:28:08 +0100

    JavaScript

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(file, body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }   
+    }); 
+  }
+});
