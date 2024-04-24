67bff273db0095f2591c4ebb6b2bb6dfb57f40ed
Tasie Ibiam <tasieibiam28@gmail.com>
2024-04-24 01:16:22 +0100

    JavaScript

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
+let characterCount = 0;
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const res = JSON.parse(body);
+    res.results.forEach((film) => {
+      film.characters.forEach((actor) => {
+        if (actor.includes(characterId)) {
+          characterCount += 1;
+        }
+      });
+    });
+    console.log(characterCount);
+  }
+});
