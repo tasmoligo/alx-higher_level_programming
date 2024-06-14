568f901e10fbdff5e860ae395fc00758cc17b74f
Tasie Ibiam <tasieibiam28@gmail.com>
2024-06-14 17:00:38 +0100

    Reads a file and writes it to standard output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+""" Reads and writes a file. """
 
 
 def read_file(filename=""):
     """ Reads a text file (UTF8) and prints it to stdout. """
-    with open(filename, "r") as f:
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     for characters in read_data:
         print(characters, end="")
