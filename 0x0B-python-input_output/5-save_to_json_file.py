3cb88892b7570a6c87648f294bd37ed6aad6e9ad
Tasie Ibiam <tasieibiam28@gmail.com>
2024-06-15 19:38:06 +0100

    JSON stuffs.

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" writes an Object. """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Writes an Object to a text file, using a JSON representation. """
+    with open(filename, "w") as f:
+        return json.dump(my_obj, f)
---
+++ 0x0B-python-input_output/ss.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "#!/usr/bin/python3" > "$1"
+echo \n "$2" >> "$1"
